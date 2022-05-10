38c779c0a9fe56bee42751f2c9cb4f78f4380c8b
Eunji Lee <eunjicious@gmail.com>
2022-05-10 22:51:50 +0900

    0_run.sh added to need_mv_to_build

--- need_mv_to_build/0_run.sh
+++ need_mv_to_build/0_run.sh
@@ -1,5 +1,15 @@
 #echo $1 $2
 
+if [[ $# -lt 2 ]]; then
+    echo “Usage: ./0_run.sh [DEVSZ_GB] [POLICY] [BUFFSZ_GB]”
+    echo “Usage: ./0_run.sh [16 64] [FIFO DAWID] [1 2 4 8]”
+    exit
+fi
+
+echo "DEVICE_SIZE $1" > femu.conf
+echo "POLICY $2" >> femu.conf
+echo "BUFFSZ_GB $3" >> femu.conf
+
 DEVICE_SIZE=$1
 if [ ${DEVICE_SIZE} -eq 64 ];then
         DEVSZ_MB=65536
@@ -10,9 +20,23 @@ elif [ ${DEVICE_SIZE} -eq 16 ];then
         BLKS_PER_PLANE=256
 fi
 
+
 POLICY=$2
-BUFF_SIZE=$3
-BUFF_THRESHOLD_RATIO=2 
+
+BUFFSZ_GB=$3
+BUFF_SIZE=1048576
+if [[ $BUFFSZ_GB == 1 ]]; then
+	BUFF_SIZE=$(expr $BUFF_SIZE / 4)
+elif [[ $BUFFSZ_GB == 2 ]]; then
+	BUFF_SIZE=$(expr $BUFF_SIZE / 2)
+elif [[ $BUFFSZ_GB == 8 ]]; then
+	BUFF_SIZE=$(expr $BUFF_SIZE * 2)
+fi
+
+echo $BUFF_SIZE
+
+
+BUFF_THRESHOLD_RATIO=1
 BUFF_THRESHOLD=$((BUFF_SIZE/BUFF_THRESHOLD_RATIO))
 
 echo $DEVSZ_MB $BLKS_PER_PLANE $POLICY $BUFF_SIZE $BUFF_THRESHOLD
